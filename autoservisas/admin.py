015462bf617534d4fea3726b33f856fa9eaf5b8d
Regis <r.petrauskui@gmail.com>
2023-08-22 10:26:52 +0300

    sukurem pirma db lentele

--- autoservisas/admin.py
+++ autoservisas/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from . models import AutomobilioModelis
+
+admin.site.register(AutomobilioModelis)
--- autoservisas/models.py
+++ autoservisas/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class AutomobilioModelis(models.Model):
+    marke = models.CharField('Marke', max_length=100)
+    modelis = models.CharField('Modelis', max_length=100)
+    metai = models.IntegerField('Metai')
+    variklis = models.CharField('Galia', max_length=100)
+
+    class Meta:
+        ordering = ['marke']
+
+    def __str__(self):
+        return f'{self.marke} {self.modelis} {self.metai} {self.variklis}'
