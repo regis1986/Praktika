58549cdcefd50dc3f34faaf1fe17045e072cba08
Regis <r.petrauskui@gmail.com>
2023-08-23 16:57:57 +0300

    4 uzduotys papildom css, base ir stasitikos

--- autoservisas/views.py
+++ autoservisas/views.py
@@ -7,14 +7,16 @@ def index(request):
     num_paslauos = Paslaugos.objects.all().count()
     num_auto_ready = Uzsakymas.objects.filter(status__exact='g').count()
     num_auto_ruosiami = Uzsakymas.objects.filter(status__exact='t').count()
-    # num_klientai = Automobilis.klientas.objects.all().count()
+    num_klientai = Uzsakymas.objects.all().count()
+    num_atliekami_darb = Uzsakymoeilutes.objects.count()
 
     context_t = {
         'num_auto_t': num_auto,
         'num_paslauos_t': num_paslauos,
         'num_auto_ready_t': num_auto_ready,
         'num_auto_ruosiami_t': num_auto_ruosiami,
-        # 'num_klientai_t': num_klientai
+        'num_klientai_t': num_klientai,
+        'num_atliekami_darb_t': num_atliekami_darb
     }
 
     return render(request, 'index.html', context=context_t)
