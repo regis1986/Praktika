cc80a9411ff24750ff161868c3927cdf247ff889
Regis <r.petrauskui@gmail.com>
2023-08-23 16:11:54 +0300

    3 uzduotys(padarom pirma puslapi su statika)

--- autoservisas/views.py
+++ autoservisas/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import AutomobilioModelis, Automobilis, Paslaugos, Uzsakymoeilutes, Uzsakymas
 
 def index(request):
-    return HttpResponse('Sukursime autoserviso appsa')
+    num_auto = Automobilis.objects.all().count()
+    num_paslauos = Paslaugos.objects.all().count()
+    num_auto_ready = Uzsakymas.objects.filter(status__exact='g').count()
+    num_auto_ruosiami = Uzsakymas.objects.filter(status__exact='t').count()
+    # num_klientai = Automobilis.klientas.objects.all().count()
+
+    context_t = {
+        'num_auto_t': num_auto,
+        'num_paslauos_t': num_paslauos,
+        'num_auto_ready_t': num_auto_ready,
+        'num_auto_ruosiami_t': num_auto_ruosiami,
+        # 'num_klientai_t': num_klientai
+    }
+
+    return render(request, 'index.html', context=context_t)
