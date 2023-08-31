8002cd875dd0185f2572a7548d7e354c39f9cac4
Regis <r.petrauskui@gmail.com>
2023-08-31 09:15:23 +0300

    sutvarkyti redirectai logino

--- praktika/urls.py
+++ praktika/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
-path('', include('autoservisas.urls')),
+    path('autoservisas/', include('autoservisas.urls')),
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='autoservisas/', permanent=True))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
 + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
